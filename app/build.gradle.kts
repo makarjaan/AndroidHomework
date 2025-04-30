9926fbaed8aa42b9360bf05977b8c6d1023017f4
makarjaan <arina.makarova.08@inbox.ru>
2025-04-30 15:57:49 +0300

    build: add hilt, okhttp, retrofit dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,9 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.gradle.secrets.plugin)
+    alias(libs.plugins.hilt.plugin)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -37,27 +40,32 @@ android {
     buildFeatures {
         compose = true
         viewBinding = true
+        buildConfig = true
+    }
+    defaultConfig {
+        buildConfigField(
+            "String",
+            "OPEN_TRIP_MAP_BASE_URL",
+            "\"https://api.opentripmap.com/0.1/ru/\""
+        )
     }
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.glide)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
+    implementation(libs.retrofit)
+    implementation(libs.okhttp)
+    implementation(libs.http.loggin.interceptor)
+    implementation(libs.androidx.fragment)
+    implementation(libs.retrofit.gson.converter)
+    implementation(libs.hilt)
+    ksp(libs.hilt.compiler)
+
 }
